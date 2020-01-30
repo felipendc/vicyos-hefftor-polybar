610c7a570670615427682d123551bfed7e19d19e
felipendc <felipendc10@gmail.com>
2020-01-30 16:16:11 -0300

    Proper fonts added with install script

---
+++ install-polybar-fonts.sh
@@ -0,0 +1,10 @@
+############################
+# Script made by Felipe Ndc
+############################
+
+# This command will install the proper fonts:
+sudo cd fonts/
+sudo cp -r fonts/* /usr/share/fonts/
+
+# This command will refresh the cache:
+fc-cache -fv
